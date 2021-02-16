a739e97117ccc3e80d15dddd92f0805ac52d1234
naisuu <julius.cathalina@gmail.com>
2021-02-16 16:48:09 +0100

    Train and store model with 0.995 val score.

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ assert data_path.exists(), "Download the necessary data from the following link:
 data_class = DataClass_normal(dataPath=data_path)
 
 print(data_class)
+
+model = DTI_Bridge(outSize=128,
+                   cHiddenSizeList=[1024],
+                   fHiddenSizeList=[1024, 256],
+                   fSize=1024, cSize=data_class.pContFeat.shape[1],
+                   gcnHiddenSizeList=[128, 128], fcHiddenSizeList=[128], nodeNum=64,
+                   hdnDropout=0.5, fcDropout=0.5, device=torch.device('cuda'))
+model.train(data_class, trainSize=512, batchSize=512, epoch=128,
+            stopRounds=-1, earlyStop=30,
+            savePath='BridgeDTI', metrics="AUC", report=["ACC", "AUC", "LOSS"],
+            preheat=0)  # Used to have learning rate (lr=0.001) as param ???
